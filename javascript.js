082468e211a89bbd86b72c2f49b8068938ee8676
cierra <138401596+cdcutshaw@users.noreply.github.com>
2024-05-31 17:40:22 -0600

    create getComputerChoice function
    return computer's "choice" by generating random number, 1-3, then assigning each number the an argument of "rock", "paper", and "scissors"

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+// function that returns random computer choice, "rock", "paper", or "scissors"
+function getComputerChoice () {
+let randomNumber = Math.floor((Math.random() * 3) + 1);
+    if (randomNumber === 1) {
+        return "rock";}
+    if (randomNumber === 2) {
+         return "paper";}
+    if (randomNumber === 3) {
+         return "scissors"}             
+}
+//console.log(getComputerChoice())
+
